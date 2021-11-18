a15fcf83e04a411d9d93ebd5432928ad349250b5
Jean-MarcCheynier <jmcheynier@gmail.com>
2021-11-18 22:52:46 +0100

    remove dist from version control

--- dist/entities/API/webhook.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ActionCode = void 0;
-var ActionCode;
-(function (ActionCode) {
-    ActionCode["CHANGE_COLOR"] = "changeUserColor";
-    ActionCode["CHANGE_LANG"] = "changeLang";
-})(ActionCode = exports.ActionCode || (exports.ActionCode = {}));
